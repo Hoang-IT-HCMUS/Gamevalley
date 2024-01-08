88e6cd8efc61bd2a7057e0d285024fb9c025b4c9
Hạo Tanker <116426754+21127272@users.noreply.github.com>
2024-01-08 13:17:03 +0700

    review.java

--- src/main/java/dev/Group08/GameValley/Model/review.java
+++ src/main/java/dev/Group08/GameValley/Model/review.java
@@ -14,4 +14,28 @@ public class review {
         downVote = 0;
         additionContent = null;
     }
+
+    review(String user, int rate, String comm, int up, int down, String[] add){
+        postUser = user;
+        rating = rate;
+        comment = comm;
+        upVote = up;
+        downVote = down;
+        additionContent = new String[add.length];
+        for(int i = 0; i < add.length; i++){
+            additionContent[i] = add[i];
+        }
+    }
+
+    review(review r){
+        this.postUser = r.postUser;
+        this.rating = r.rating;
+        this.comment = r.comment;
+        this.upVote = r.upVote;
+        this.downVote = r.downVote;
+        this.additionContent = new String[r.additionContent.length];
+        for(int i = 0; i < r.additionContent.length; i++){
+            this.additionContent[i] = r.additionContent[i];
+        }
+    }
 }
