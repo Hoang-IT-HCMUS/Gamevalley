2937236394b6c9f3aa61e739bac54c9fa25d6f43
Hạo Tanker <116426754+21127272@users.noreply.github.com>
2024-01-08 12:52:45 +0700

    mongoDB.java

--- src/main/java/dev/Group08/GameValley/Repository/mongoDB.java
+++ src/main/java/dev/Group08/GameValley/Repository/mongoDB.java
@@ -2,26 +2,113 @@ import com.mongodb.*;
 public class mongoDB {
     public static MongoClient mongoClient;
     public static DB database;
-    public static DBCollection test;
+    public static DBCollection collection;
 
-    public static void main(String[] args) throws Exception {
-        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-        database = mongoClient.getDB("mongoTutorial");
-        test = database.getCollection("testing");
-        user t = new user();
-        t.setEmail("abc");
-        t.setPhone("0123456789");
-        t.setPassword("HaoTanker");
-        test.insert(convert(t));
-        DBObject query = new BasicDBObject("XP", 11);
-        DBCursor cursor = test.find(query);
-        System.out.println(cursor.one());
-
-        System.out.println(jpgToBin.encodeImage("C:\\Users\\LENOVO\\Downloads\\komi.jpg",
-                                                "C:\\Users\\LENOVO\\Downloads\\img.txt"));
+    public static void connectDB() throws Exception {
+        mongoClient = new MongoClient(new MongoClientURI("mongoDB://localhost27017"));
+        database = mongoClient.getDB("db");
     }
 
-    public static DBObject convert(user test){
-        return new BasicDBObject("XP", test.getEmail()).append("Timer", test.getPassword()).append("memberID", test.getPhone());
+    public static void connectUser() throws Exception {
+        if(database == null)
+            connectDB();
+        collection = database.getCollection("user");
+    }
+
+    public static void connectProduct() throws Exception {
+        if(database == null)
+            connectDB();
+        collection = database.getCollection("product");
+    }
+
+    public static void connectReview() throws Exception {
+        if(database == null)
+            connectDB();
+        collection = database.getCollection("review");
+    }
+
+    public static void connectPic()throws Exception {
+        if(database == null)
+            connectDB();
+        collection = database.getCollection("picture");
+    }
+
+    public static void insertUser(user t) throws Exception {
+        connectUser();
+        collection.insert(convertUser(t));
+    }
+
+    public static void insertProduct(product p) throws Exception {
+        connectProduct();
+        collection.insert(convertProduct(p));
+    }
+
+    public static void insertReview(review r) throws Exception {
+        connectReview();
+        collection.insert(convertReview(r));
+    }
+
+    public static void insertPic(String name, String path) throws Exception {
+        connectPic();
+        collection.insert(convertPic(path, name));
+    }
+
+    public static DBCursor findUserByName(String name) throws Exception {
+        connectUser();
+        DBObject query = new BasicDBObject("Username", name);
+        return collection.find(query);
+    }
+
+    public static DBCursor findReviewByUser(String name){
+        DBObject query = new BasicDBObject("postUser", name);
+        return collection.find(query);
+    }
+
+    public static DBCursor findProductByName(String name){
+        DBObject query = new BasicDBObject("pName", name);
+        return collection.find(query);
+    }
+
+    public static DBCursor findPicByName(String name){
+        DBObject query = new BasicDBObject("Name", name);
+        return collection.find(query);
+    }
+
+    public static DBObject convertUser(user test){
+        return new BasicDBObject
+                ("Username", test.username)
+                .append("Email", test.getEmail())
+                .append("uID", test.uID)
+                .append("password", test.getPassword())
+                .append("upvote", test.upVote)
+                .append("downvote", test.downVote);
+    }
+
+    public static DBObject convertProduct(product test){
+        return new BasicDBObject
+                ("pName", test.pName)
+                .append("ID", test.pID)
+                .append("Tags", test.pTag)
+                .append("Links", test.pLinks)
+                .append("Price", test.pPrice)
+                .append("Creator", test.pCreator)
+                .append("Content", test.content)
+                .append("Review", test.pReview)
+                .append("Popularity", test.pPopularity);
+    }
+
+    public static DBObject convertReview(review test){
+        return new BasicDBObject
+                ("postUser", test.postUser)
+                .append("Rating", test.rating)
+                .append("Comment", test.comment)
+                .append("Upvote", test.upVote)
+                .append("Downvote", test.downVote)
+                .append("Content", test.additionContent);
+    }
+
+    public static DBObject convertPic(String path, String name) throws Exception {
+        String pic = jpgToBin.encodeImage(path);
+        return new BasicDBObject("picString", pic).append("Name", name);
     }
 }
