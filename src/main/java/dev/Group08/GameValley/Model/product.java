a0bc23469df50c1ff6761c05b210b296ff6ed7cd
Hạo Tanker <116426754+21127272@users.noreply.github.com>
2024-01-08 13:38:50 +0700

    product.java

--- src/main/java/dev/Group08/GameValley/Model/product.java
+++ src/main/java/dev/Group08/GameValley/Model/product.java
@@ -20,4 +20,28 @@ public class product {
         pReview = null;
         pPopularity = 0;
     }
+
+    product(String name, String ID, tag[] t, String[] link, float price, String creator, String[] cont, review[] r, int pop){
+        pName = name;
+        pID = ID;
+        pTag = new tag[t.length];
+        for(int i = 0; i < t.length; i++){
+            pTag[i] = t[i];
+        }
+        pLinks = new String[link.length];
+        for(int i = 0; i < link.length; i++){
+            pLinks[i] = link[i];
+        }
+        pPrice = price;
+        pCreator = creator;
+        content = new String[cont.length];
+        for(int i = 0; i < cont.length; i++){
+            content[i] = cont[i];
+        }
+        pReview = new review[r.length];
+        for(int i = 0; i < r.length; i++){
+            pReview[i] = r[i];
+        }
+        pPopularity = pop;
+    }
 }
