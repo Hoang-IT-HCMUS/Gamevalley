c3930cbb9704836b6003b047f60d6a4878e16b25
hoangvta <hoangutaphan@gmail.com>
2024-01-10 01:47:40 +0700

    #update

--- src/main/java/dev/Group08/GameValley/Repository/ReviewRepository.java
+++ src/main/java/dev/Group08/GameValley/Repository/ReviewRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends MongoRepository<ReviewModel, String> {
     @Query("{ 'pID': { $regex: ?0 } }")
-    public ReviewModel getByProduct(String pID);
+    public List<ReviewModel> getByProduct(String pID);
 
     @Query("{ 'uID': { $regex: ?0 } }")
-    public ReviewModel getByUser(String uID);
+    public List<ReviewModel> getByUser(String uID);
 }
--- src/main/java/dev/Group08/GameValley/Service/GameProductService.java
+++ src/main/java/dev/Group08/GameValley/Service/GameProductService.java
@@ -7,14 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 public class GameProductService {
-//    @Autowired
-//    private ReviewRepository reviewRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
 
-    public void addReview(String pID, ReviewModel review) {
+    public void addReview(String pID, String uID, String content) {
+        ReviewModel newReview = new ReviewModel();
 
+        newReview.setContent(content);
+        newReview.setpID(pID);
+        newReview.setuID(uID);
+        newReview.setUpvote(0);
+        newReview.setDownvote(0);
+
+        reviewRepository.save(newReview);
     }
 
-    public List<ReviewModel> getReviews() {
-        return null;
+    public List<ReviewModel> getReviews(String pID) {
+        return reviewRepository.getByProduct(pID);
     }
 }
